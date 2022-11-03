5c05e996c02053de555b0fc782fb4f50d080daf4
Rogério Farias Otto <rogerio.ocupado@gmail.com>
2022-11-03 17:58:15 -0300

    textura com luz

--- Test/test.js
+++ Test/test.js
@@ -146,6 +146,7 @@ void main() {
 var vst = `
 #version 300 es
 in vec4 a_position;
+in vec3 a_normal;
 in vec2 a_texcoord;
  
 uniform vec3 u_lightWorldPosition;
@@ -154,6 +155,11 @@ uniform vec3 u_viewWorldPosition;
 uniform mat4 u_matrix;
 uniform mat4 u_world;
 uniform mat4 u_worldInverseTranspose;
+
+out vec3 v_normal;
+
+out vec3 v_surfaceToLight;
+out vec3 v_surfaceToView;
  
 // a varying to pass the texture coordinates to the fragment shader
 out vec2 v_texcoord;
@@ -164,6 +170,15 @@ void main() {
  
   // Pass the texcoord to the fragment shader.
   v_texcoord = a_texcoord;
+
+  // Pass the color to the fragment shader.
+  v_normal = mat3(u_worldInverseTranspose) * a_normal;
+
+  vec3 surfaceWorldPosition = (u_world * a_position).xyz;
+
+  v_surfaceToLight = u_lightWorldPosition - surfaceWorldPosition;
+
+  v_surfaceToView = u_viewWorldPosition - surfaceWorldPosition;
 }
 `;
 
@@ -173,15 +188,36 @@ precision highp float;
  
 // Passed in from the vertex shader.
 in vec2 v_texcoord;
- 
+in vec3 v_normal;
+in vec3 v_surfaceToLight;
+in vec3 v_surfaceToView;
+
 // The texture.
 uniform sampler2D u_texture;
+uniform float u_shininess;
  
 out vec4 outColor;
  
 void main() {
-   outColor = texture(u_texture, v_texcoord);
-}
+   
+  vec3 normal = normalize(v_normal);
+
+  vec3 surfaceToLightDirection = normalize(v_surfaceToLight);
+  vec3 surfaceToViewDirection = normalize(v_surfaceToView);
+  vec3 halfVector = normalize(surfaceToLightDirection + surfaceToViewDirection);
+
+  float light = dot(v_normal, surfaceToLightDirection);
+  float specular = 0.0;
+
+  if (light > 0.0) {
+    specular = pow(dot(normal, halfVector), u_shininess);
+  }
+
+  outColor = texture(u_texture, v_texcoord);
+
+  outColor.rgb *= light;
+  outColor.rgb += specular;
+} 
 `;
 
 var TRS = function() {
